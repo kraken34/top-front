d90b6c7f7922a30dec500b4b992145f6c2481337
Balcells Edouard <balcellsedouard1@gmail.com>
2018-11-30 17:11:35 +0100

    header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { BandeauComponentComponent } from './bandeau-component/bandeau-component.component';
+import { AvisComponentComponent } from './avis-component/avis-component.component';
+import { CollegueComponentComponent } from './collegue-component/collegue-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BandeauComponentComponent,
+    AvisComponentComponent,
+    CollegueComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MDBBootstrapModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/collegue-component/collegue-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collegue-component',
+  templateUrl: './collegue-component.component.html',
+  styleUrls: ['./collegue-component.component.scss']
+})
+export class CollegueComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
