a668d4e347bbdf2575d0812b12d5ede710ccc85a
Simon-Benezet <simon.benezet@hotmail.fr>
2018-11-30 13:24:36 +0100

    5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,43 +1,44 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'ngbd-modal-basic',
+  templateUrl: './modal-basic.html'
 })
-export class AppComponent {
-  title = 'top-collegues-front';
+export class NgbdModalBasic {
+  closeResult: string;
+
+  constructor(private modalService: NgbModal) {}
+
+  open(content) {
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 }
 
 
-// import {Component} from '@angular/core';
 
-// import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
+// import { Component } from '@angular/core';
 
 // @Component({
-//   selector: 'ngbd-modal-basic',
-//   templateUrl: './modal-basic.html'
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
 // })
-// export class NgbdModalBasic {
-//   closeResult: string;
-
-//   constructor(private modalService: NgbModal) {}
-
-//   open(content) {
-//     this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
-//       this.closeResult = `Closed with: ${result}`;
-//     }, (reason) => {
-//       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-//     });
-//   }
-
-//   private getDismissReason(reason: any): string {
-//     if (reason === ModalDismissReasons.ESC) {
-//       return 'by pressing ESC';
-//     } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-//       return 'by clicking on a backdrop';
-//     } else {
-//       return  `with: ${reason}`;
-//     }
-//   }
-// }
+export class AppComponent {
+  title = 'top-collegues-front';
+}
