d75769b942c668ccde63b95c47f4e5ff4b355ced
Bastianelli_Matthieu <m.bastianelli@laposte.net>
2018-12-03 10:14:32 +0100

    avis in collegue

--- src/app/models.ts
+++ src/app/models.ts
@@ -5,9 +5,12 @@ export class Collegue{
     nom:string;
     score:number=0;
     photo:string;
+    
 
     constructor(nom:string, photo:string){
         this.nom = nom;
         this.photo = photo;
     }
 }
+
+enum avis {AIMER,DETESTER};
