dee0bffce947b1be5d6106f2a0fd83132dc52d96
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-04 16:57:53 +0100

    9

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,9 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  // collegue: Collegue = new Collegue("https://cdn-images-1.medium.com/max/1200/0*pAdZLfSqNrMZAAPA.jpg", "Didier", Avis, 0);
-
   constructor() {
   }
 }
