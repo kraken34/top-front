66118920b6334e11231fe0dd3f60e657fc5d4698
Champetier <todave11@gmail.com>
2018-12-05 10:09:02 +0100

    hahahav2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,12 @@ import { CollegueService } from './services/collegue.service';
 export class AppComponent {
   title = 'top-front';
   expandable: Collegue[];
+  votes:Vote[];
 
   constructor(private pService: CollegueService) {
     this.pService.listerCollegues().then(listCollegues => this.expandable = listCollegues);
+    this.votes=[new Vote(this.expandable[0],Avis.AIMER),new Vote(this.expandable[1],Avis.DETESTER)]
   }
-  votes:Vote[]=[new Vote(this.expandable[0],Avis.AIMER),new Vote(this.expandable[1],Avis.DETESTER)]
+  
 
 }
