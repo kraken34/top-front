5433efadf1e784f1f72b4dab104da73576fc5bde
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-05 10:43:22 +0100

    9

--- src/app/services/collegue.service.ts
+++ src/app/services/collegue.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from 'src/environments/environment.prod';
+import { environment } from 'src/environments/environment';
 import { Collegue, Avis } from '../models';
 
 const URL_BACKEND = environment.backendUrl;
@@ -14,7 +14,7 @@ export class CollegueService {
 
   listerCollegues(): Promise<Collegue[]> {
     //http://localhost:8080
-    return this._http.get('/collegues').toPromise()
+    return this._http.get('https://simon-benezet-top-collegues.herokuapp.com/' + '/collegues').toPromise()
       .then((tabCol: any[]) => tabCol
         .map(col => new Collegue(col.photo, col.nom, col.score)))
   }
