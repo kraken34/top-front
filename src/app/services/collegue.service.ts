954bd4cb3bc154e82e46c9da7b8eebb4e02a3b6a
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-05 12:06:53 +0100

    9

--- src/app/services/collegue.service.ts
+++ src/app/services/collegue.service.ts
@@ -1,25 +1,31 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import { Collegue } from '../models';
+import { Avis, Collegue } from '../models';
 
 const URL_BACKEND = environment.backendUrl;
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class CollegueService {
 
+
   constructor(private _http: HttpClient) { }
 
   listerCollegues(): Promise<Collegue[]> {
-    //http://localhost:8080
     return this._http.get(URL_BACKEND + '/collegues').toPromise()
       .then((tabCol: any[]) => tabCol
         .map(col => new Collegue(col.photo, col.nom, col.score)))
   }
 
-  // donnerUnAvis(unCollegue: Collegue, avis: Avis): Promise<Collegue[]> {
-  //   return this._http.patch('http://localhost:8080/collegues').toPromise()
-  //     .then()
-  // }
+  donnerUnAvis(unCollegue: Collegue, avis: Avis): Promise<Collegue> {
+    return this._http.patch(URL_BACKEND + '/collegues', {"action": avis.toString}, httpOptions)
+    .toPromise().then((c: Collegue) => c)
+  }
 }
