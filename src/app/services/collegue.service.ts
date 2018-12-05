e4c78abac89941354f94fabd1be15252acca73ac
florian <flo811@msn.com>
2018-12-05 10:50:17 +0100

    j

--- src/app/services/collegue.service.ts
+++ src/app/services/collegue.service.ts
@@ -3,7 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Collegue, Avis } from '../model';
 import { environment } from 'src/environments/environment';
 
-const URL_BACKEND = environment.backendUrl
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +14,14 @@ export class CollegueService {
   constructor(private _http: HttpClient) { }
 
   listerCollegues(): Promise<Collegue[]> {
-    return this._http.get("http://florian-top-collegues.herokuapp.com/collegues").toPromise()
+    return this._http.get(environment.backendUrl + "collegues").toPromise()
       .then((tabColServeur: any[]) => tabColServeur.map(cServeur => new Collegue(cServeur.photo, cServeur.pseudo, cServeur.score)))
   }
 
   donnerUnAvis(unCollegue: Collegue, avis: Avis): Promise<Collegue> {
     const jsonAvis = { "action": avis == Avis.AIMER ? "AIMER" : "DETESTER" }
 
-    return this._http.patch("http://florian-top-collegues.herokuapp.com/collegues/" + unCollegue.pseudo, jsonAvis).toPromise()
+    return this._http.patch(environment.backendUrl + "collegues/" + unCollegue.pseudo, jsonAvis).toPromise()
       .then((collegue: any) => new Collegue(collegue.photo, collegue.pseudo, collegue.score))
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://flo811.github.io/top-front/'
+  backendUrl: 'http://florian-top-collegues.herokuapp.com/'
 };
