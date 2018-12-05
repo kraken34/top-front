9f03ca4f12a381af9fa93437440e4166ec592898
florian <flo811@msn.com>
2018-12-05 09:43:11 +0100

    m

--- src/app/services/collegue.service.ts
+++ src/app/services/collegue.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Collegue, Avis } from '../model';
-import { environment } from 'src/environments/environment';
+import { environment } from 'src/environments/environment.prod';
 
-const URL_BACKEND = environment.backendUrl
+const URL_BACKEND = environment
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +14,14 @@ export class CollegueService {
   constructor(private _http: HttpClient) { }
 
   listerCollegues(): Promise<Collegue[]> {
-    return this._http.get("http://localhost:8080/collegues").toPromise()
+    return this._http.get("https://flo811.github.io/top-front/collegues").toPromise()
       .then((tabColServeur: any[]) => tabColServeur.map(cServeur => new Collegue(cServeur.photo, cServeur.pseudo, cServeur.score)))
   }
 
   donnerUnAvis(unCollegue: Collegue, avis: Avis): Promise<Collegue> {
     const jsonAvis = { "action": avis == Avis.AIMER ? "AIMER" : "DETESTER" }
 
-    return this._http.patch("http://localhost:8080/collegues/" + unCollegue.pseudo, jsonAvis).toPromise()
+    return this._http.patch("https://flo811.github.io/top-front/collegues/" + unCollegue.pseudo, jsonAvis).toPromise()
       .then((collegue: any) => new Collegue(collegue.photo, collegue.pseudo, collegue.score))
   }
 }
