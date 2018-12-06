cd17a74fa142ddd989ad8201ac0114dbf0138755
florian <flo811@msn.com>
2018-12-06 16:17:49 +0100

    0

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router';
 
 export class FormulaireComponent implements OnInit {
 
-  errorMatricule: boolean=false
+  errorMatricule: boolean = false
 
   matricule: string
   pseudo: string
