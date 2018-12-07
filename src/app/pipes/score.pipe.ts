f601e73513e96fd9c54c227359cc11f862902b4b
Simon-Benezet <simon.benezet@hotmail.fr>
2018-12-07 13:48:03 +0100

    9

--- src/app/pipes/score.pipe.ts
+++ src/app/pipes/score.pipe.ts
@@ -6,7 +6,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ScorePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return null;
+    if (value > 0) {
+      return `<span class="scorePlus score">+${value}</span>`
+    } else if (value == 0) {
+      return `<span class="score0 score">${value}</span>`
+    } else {
+      return `<span class="scoreMoins score">${value}</span>`
+    }
   }
-
 }
